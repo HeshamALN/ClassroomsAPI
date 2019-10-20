4e5cca4e41096317c78634fffcfc5e14826ce5e6
Hesham Al Nukhailan <halnukhailan@gmail.com>
2019-10-20 21:01:07 +0300

    classroomapi task

--- classrooms/urls.py
+++ classrooms/urls.py
@@ -34,10 +34,6 @@ urlpatterns = [
 
 	path('api/login/', TokenObtainPairView.as_view()), #done i think
 
-  	path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    
 ]
 
 if settings.DEBUG:
